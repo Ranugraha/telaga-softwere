b298d77b5fd34025af84a0598f79301b1148bb27
Ranugraha <awan72.ra@gmail.com>
2023-01-25 09:54:02 +0700

    dropdown menu
    initial

--- components/NavbarMenu.tsx
+++ components/NavbarMenu.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { DropdownIcon } from "./images/icon";
 
 type Props = {
@@ -8,12 +8,26 @@ type Props = {
 };
 
 function NavbarMenu(props: Props) {
+  const [state, setState] = useState(false);
+  const toogle = () => {
+    setState(!state);
+  };
   return (
     <li className="flex flex-row items-center ">
-      <Link href={props.href} className="flex flex-row">
-        {props.menuName}
-        <DropdownIcon className="m-2" />
-      </Link>
+      <button onClick={toogle} className="">
+        <Link href={props.href} className="flex flex-row transition">
+          {props.menuName}
+
+          <DropdownIcon
+            className={
+              state
+                ? "m-2 transition duration-500"
+                : "t m-2 rotate-180 transition duration-500"
+            }
+          />
+        </Link>
+      </button>
+      <h1 className={state ? "hidden" : ""}>read</h1>
     </li>
   );
 }
--- components/OutPartner.tsx
+++ components/OutPartner.tsx
@@ -4,9 +4,9 @@ type Props = {};
 
 export default function OutPartner({}: Props) {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <p>Trusted By over 150,000 Companies including</p>
-      <div className="grid grid-cols-4 gap-4 ">
+      <div className="grid grid-cols-4 gap-4">
         <div>1</div>
         <div>1</div>
         <div>1</div>
